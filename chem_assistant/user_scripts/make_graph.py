b79b19606954a249054e3932820698d39028a88e
tommason14 <tom.mason14@gmail.com>
2019-02-06 10:36:26 +1100

    gaussian plot opt

--- chem_assistant/user_scripts/make_graph.py
+++ chem_assistant/user_scripts/make_graph.py
@@ -3,6 +3,14 @@
 
 __all__ = ['plot_opt']
 
+from ..interfaces.gamess_results import GamessResults
+from ..interfaces.psi_results import PsiResults
+from ..interfaces.gaussian_results import GaussianResults
+from .grep_results import get_results_class
+
+import os
+import re
+
 """
 File: make_graph.py 
 Author: Tom Mason
@@ -12,15 +20,29 @@ Description: Graph of energy vs iteration of geom opt in GAMESS
 """
 
 def get_data(file):
+    r = get_results_class(file)
     iters = []
     energies = []
-    with open(file, "r") as f:
-        for line in f.readlines():
-            if 'NSERCH:' in line:
-                _, iteration, _, energy, *_ = line.split()
-                iters.append(int(iteration))
-                energies.append(float(energy))
+    if isinstance(r, GamessResults):
+        with open(file, "r") as f:
+            for line in f.readlines():
+                if 'NSERCH:' in line:
+                    _, iteration, _, energy, *_ = line.split()
+                    iters.append(int(iteration))
+                    energies.append(float(energy))
+    elif isinstance(r, GaussianResults):
+        energies_per_iter = []
+        with open(file, "r") as f:
+            for line in f.readlines():
+                if 'Step number' in line:
+                    iters.append(int(line.split()[2]))
+                if re.search('^\sE=\s*-?[0-9]*.[0-9]*', line):
+                    energies_per_iter.append(float(line.split()[1]))
+                if 'Step number' in line: # find next iteration
+                    energies.append(energies_per_iter[-1])
+                    energies_per_iter.clear()
     return {'iters': iters, 'energies': energies} 
+
 
 def plot_opt(file):
 
