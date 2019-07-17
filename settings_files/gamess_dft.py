557e963d2288ec023a5a4e692875ee2486021128
tommason14 <tom.mason14@gmail.com>
2019-07-17 11:58:42 +1000

    added settings files

---
+++ settings_files/gamess_dft.py
@@ -0,0 +1,8 @@
+from chem_assistant import Settings
+sett = Settings()
+
+sett.input.mp2 = None
+sett.input.contrl.mplevl = None
+sett.input.contrl.dfftyp = 'm062x'
+sett.input.dft.method = 'grid'
+
