610ced6377c648fc4c5628696d6599c9c67f708b
tommason14 <tom.mason14@gmail.com>
2019-07-23 10:32:26 +1000

    supercomp now uses hostname not pwd

--- chem_assistant/templates/meta_files/gamess/freq/meta.py
+++ chem_assistant/templates/meta_files/gamess/freq/meta.py
@@ -2,5 +2,5 @@ from chem_assistant import GamessJob, Settings
 
 s = Settings()
 s.input.contrl.runtyp = 'hessian'
-s.input.force.method = 'seminum'
+# s.input.force.method = 'seminum'
 GamessJob(fmo = True, frags_in_subdir = True, using = 'equil.xyz', settings = s)
