21b7d4c9ecf91ee6be3ad5b47ce743829a970717
tommason14 <tom.mason14@gmail.com>
2019-07-31 17:56:05 +1000

    added geodesics flag, and accompanying code

--- chem_assistant/user_scripts/grep_results.py
+++ chem_assistant/user_scripts/grep_results.py
@@ -1,4 +1,5 @@
 __all__ = ['create_extra_jobs',
+           'geodesics',
            'get_h_bonds',
            'get_results_class',
            'parse_results',
@@ -6,6 +7,7 @@ __all__ = ['create_extra_jobs',
            'search_for_coords',
            'thermochemistry']
 
+from ..core.atom import Atom
 from ..core.molecule import Molecule
 from ..core.thermo import thermo_data
 from ..core.utils import (read_file,
@@ -18,6 +20,7 @@ from ..interfaces.psi_results import PsiResults
 from ..interfaces.gaussian_results import GaussianResults
 from .make_files_meta import make_files_from_meta
 import os
+import re
 
 """
 File: grep_results.py
@@ -345,3 +348,63 @@ def get_h_bonds(dir):
         print()
         write_csv_from_nested(output, col_names=('File', 'Path', 'Molecule1', 'Atom1',
                                                  'Molecule2', 'Atom2', 'Length (Å)', 'Angle (°)'), filename='hbonds.csv')
+
+
+def file_is_gamess(file):
+    """ Check first line of file for 'rungms' string """
+    with open(file, 'r') as f:
+        return 'rungms' in f.readline()
+
+def geodesics(dir):
+    """
+    Recursively pulls geodesic charges from GAMESS calculations.
+    Writes to `charges.csv` if desired
+    """
+
+    atom_regex = '^\s[A-Za-z]{1,2}\s*[0-9]*.[0-9]*(\s*-?[0-9]*.[0-9]*){3}$'
+    charge_regex = '^\s[A-Za-z]{1,2}(\s*-?[0-9]*.[0-9]*){2}$'
+
+    results = []
+
+    files = get_files(dir, ['log', 'out'])
+
+    for logfile in files:
+        if file_is_gamess(logfile):
+            path, filename = os.path.split(logfile)
+            inpfile = logfile[:-3] + 'inp'
+
+            res = []
+            assigned = []
+
+            for line in read_file(inpfile):
+                if re.search(atom_regex, line):
+                    sym, atnum, x, y, z = line.split()
+                    x, y, z = map(float, (x, y, z))
+                    res.append([path, Atom(sym, coords = (x, y, z))]) # new key for each coord
+            found = False
+            counter = 0
+            for line in read_file(logfile):
+                if 'NET CHARGES:' in line:
+                    found = True
+                if 'RMS DEVIATION' in line:
+                    break
+                if found: 
+                    if re.search(charge_regex, line):
+                        res[counter].append(float(line.split()[1]))
+                        counter += 1
+            coords = [atom[1] for atom in res]
+            mol = Molecule(atoms = coords)
+            mol.separate()
+            # bad practice, should use for i, j in zip(mol.coords, res):
+            for atom, r in zip(mol.coords, res):
+                path, _, geodesic_charge = r
+                results.append([path, atom.index, atom.symbol, geodesic_charge, atom.x, atom.y, atom.z, f"{mol.fragments[atom.mol]['name']}_{atom.mol}"])
+
+    # nested list (one level) to dict
+    data = {}
+    keys = ('Path', 'Index', 'Element', 'Geodesic', 'Rx', 'Ry', 'Rz', 'Fragment')
+    for index, value in enumerate(keys):
+        data[value] = [val[index] for val in results]
+    responsive_table(data, strings=[1, 3, 8], min_width=10)
+
+    write_csv_from_nested(results, col_names = keys, filename='charges.csv')
