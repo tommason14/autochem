9ad2da808ed95068203d6fd899226174cddb0ad3
Tom Mason <tommason14@gmail.com>
2019-08-11 17:43:07 +1000

    added settings with job constructors

---
+++ settings_files/create_jobs/gamess_dft.py
@@ -0,0 +1,12 @@
+from chem_assistant import Settings, GamessJob
+import glob
+
+xyz = glob.glob('*xyz')[0]
+
+sett = Settings()
+sett.input.mp2 = None
+sett.input.contrl.mplevl = None
+sett.input.contrl.dfttyp = 'm062x'
+sett.input.dft.method = 'grid'
+
+GamessJob(using=xyz, fmo=True, frags_in_subdir=True, settings=sett)
---
+++ settings_files/create_jobs/gamess_geodesic.py
@@ -0,0 +1,17 @@
+from chem_assistant import Settings, GamessJob
+import glob
+
+xyz = glob.glob('*xyz')[0]
+
+sett = Settings()
+sett.input.basis.gbasis='cct'
+sett.input.contrl.runtyp='energy'
+sett.input.elpot.iepot=1
+sett.input.elpot.where='pdc'
+sett.input.pdc.ptsel='geodesic'
+
+# rm defaults
+sett.input.contrl.mplevl=None
+sett.input.mp2=None
+
+GamessJob(using=xyz, settings=sett)
