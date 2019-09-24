888fa7d1edf5ccd3d6c3674490f8142fb296932c
tommason14 <tom.mason14@gmail.com>
2019-09-24 18:45:08 +1000

    added hf energy collection

--- chem_assistant/interfaces/psi_results.py
+++ chem_assistant/interfaces/psi_results.py
@@ -22,13 +22,31 @@ class PsiResults(Results):
         return complete
 
     def get_runtype(self):
+        """
+        Returns runtype. For example, for MP2 single points, the line `energy('mp2')` is used. 
+        This method returns the string 'energy'.
+        """
         for line in self.read():
-            #regex for energy('mp2') or optimize('scf', dertype='hess') (any number of k-v pairs)
+            # need regex for energy('mp2') or optimize('scf', dertype='hess') (any number of k-v pairs)
                 if re.search("[A-z]*\('[A-z0-9]*'(.?\s*[A-z]*='[A-z]*')*\)", line):
                     if re.search("[A-z]*\('[A-z0-9]*'\)", line): #energy('mp2')
                         return line.split('(')[0]
                     else: #optimize('scf', dertype='hess'......)
                         return line.split('(')[0] #add to this later, using the collect additional data
+
+    @property
+    def energy_type(self):
+        """
+        Returns energy type. For example, for MP2 single points, the line `energy('mp2')` is used. 
+        This method returns the string 'mp2'.
+        """
+        for line in self.read():
+            # need regex for energy('mp2') or optimize('scf', dertype='hess') (any number of k-v pairs)
+                if re.search("[A-z]*\('[A-z0-9]*'(.?\s*[A-z]*='[A-z]*')*\)", line):
+                    if re.search("[A-z]*\('[A-z0-9]*'\)", line): #energy('mp2')
+                        return re.search("[A-z]*\('([A-z0-9]*)'\)", line).group(1)
+                    # else: #optimize('scf', dertype='hess'......)
+                        # return line.split('(')[0] #add to this later, 
 
     def is_optimisation(self):
         return self.get_runtype() == 'optimize'
@@ -40,56 +58,80 @@ class PsiResults(Results):
         return self.get_runtype() == 'frequency'
 
     def get_data(self):
-        HF = ''
-        opp = ''
-        same = ''
-        basis = ''
-        MP2 = '' 
-        # with open(filepath, "r") as f:
-        #     for line in f.readlines():
+        """
+        Returns job data: filename, filepath, basis set, HF/DFT energy, and MP2 opposite
+        and same spin parameters if relevant.
+        """  
+        if self.energy_type == 'scf':
+            return self.scf_data()
+        
+        elif self.energy_type == 'mp2':
+            return self.mp2_data() 
+
+    @property
+    def basis(self):
+        """
+        Returns basis set.
+        """ 
         for line in self.read():
             if re.search('basis\s\w*(\-?\w*){1,2}$', line):
-                basis = line.split()[-1]
+                return line.split()[-1]
+
+    @property
+    def total_energy(self):
+        """
+        Returns total energy, printed for scf calculations.
+        """
+        total=''
+        for line in self.read():
+            if 'Total Energy =' in line:
+                total = float(line.split('=')[1].strip())
+        return total
+
+    def scf_data(self):
+        """
+        Return data for scf calculations. 
+        Note the NAs returned are because of no MP2 spin parameters.
+        """
+        return self.file, self.path, self.basis, self.total_energy, 'NA', 'NA'
+
+    @property
+    def hf_energy_for_mp2(self):
+        """
+        Returns 'reference energy' from MP2 calculations.
+        """
+        HF=''
+        for line in self.read():
             if 'Reference Energy          =' in line:
                 HF = float(line.split('=')[1].split()[0].strip())
-            elif 'Same-Spin Energy          =' in line:
-                same = float(line.split('=')[1].split()[0].strip())
-            elif 'Opposite-Spin Energy      =' in line:
+        return HF
+
+    @property
+    def mp2_opp(self):
+        """
+        Returns MP2 opposite spin energy.
+        """
+        opp=''
+        for line in self.read():
+            if 'Opposite-Spin Energy      =' in line:
                 opp = float(line.split('=')[1].split()[0].strip())
+        return opp 
         
-        SRS = {}
-        SRS['c_os'] = {}
-        SRS['c_os']['ccd']         = 1.752
-        SRS['c_os']['cc-pvdz']     = 1.752
-        SRS['c_os']['cct']         = 1.64
-        SRS['c_os']['cc-pvtz']     = 1.64
-        SRS['c_os']['ccq']         = 1.689
-        SRS['c_os']['cc-pvqz']     = 1.689
-        SRS['c_os']['accd']        = 1.372
-        SRS['c_os']['aug-cc-pvdz'] = 1.372
-        SRS['c_os']['acct']        = 1.443
-        SRS['c_os']['aug-cc-pvtz'] = 1.443
-        SRS['c_os']['accq']        = 1.591
-        SRS['c_os']['aug-cc-pvqz'] = 1.591
-
-        SRS['c_ss'] = {}
-        SRS['c_ss']['ccd']         = 0
-        SRS['c_ss']['cc-pvdz']     = 0
-        SRS['c_ss']['cct']         = 0
-        SRS['c_ss']['cc-pvtz']     = 0
-        SRS['c_ss']['ccq']         = 0
-        SRS['c_ss']['cc-pvqz']     = 0
-        SRS['c_ss']['accd']        = 0
-        SRS['c_ss']['aug-cc-pvdz'] = 0
-        SRS['c_ss']['acct']        = 0
-        SRS['c_ss']['aug-cc-pvtz'] = 0
-        SRS['c_ss']['accq']        = 0
-        SRS['c_ss']['aug-cc-pvqz'] = 0
-
-
-        c_os = SRS['c_os'][basis.lower()]
-        c_ss = SRS['c_ss'][basis.lower()]
+    @property
+    def mp2_same(self):
+        """
+        Returns MP2 same spin energy.
+        """
+        same=''
+        for line in self.read():
+            if 'Same-Spin Energy          =' in line:
+                same = float(line.split('=')[1].split()[0].strip())
+        return same
+    
+    def mp2_data(self):
+        """
+        Returns data for MP2 calculations: filename, filepath, basis set, hf energy, opp spin energy, same
+        spin energy.
+        """
         
-        # MP2 = HF + c_os * opp + c_ss * same
-        
-        return self.file, self.path, basis, HF, opp, same
+        return self.file, self.path, self.basis, self.hf_energy_for_mp2, self.mp2_opp, self.mp2_same
