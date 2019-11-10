84bf4648442d7b7f61b5c51e4870b8a41c16f5ed
tommason14 <tom.mason14@gmail.com>
2019-11-11 10:34:26 +1100

    gauss spec settings

---
+++ settings_files/gauss_spec.py
@@ -0,0 +1,8 @@
+from chem_assistant import Settings
+
+sett=Settings()
+sett.input.method='wB97XD'
+sett.input.basis='aug-cc-pVTZ'
+# sett.meta.time='48:00:00'
+# sett.meta.mem='96gb'
+# sett.meta.nproc='24'
