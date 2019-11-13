8210eb549940a04b3664296b5031fc0c957352d9
tommason14 <tom.mason14@gmail.com>
2019-11-13 16:06:58 +1100

    clean up

--- chem_assistant/interfaces/orca_results.py
+++ chem_assistant/interfaces/orca_results.py
@@ -45,59 +45,6 @@ class OrcaResults(Results):
                 return True
         return False
 
-    # def get_equil_coords(self, output = None):
-    #     found_equil = False
-    #     found_some = False
-    #     regex = "^(\s*[0-9]*){3}(\s*-?[0-9]{1,3}.[0-9]*){3}$"
-    #     coords = []
-    #     some_coords = []
-    #     par_dir = []
-    #     # print(self.log)
-    #     for part in self.path.split('/'):
-    #         if part not in ('opt', 'spec', 'hess'):
-    #             par_dir.append(part)
-    #         else:
-    #             break
-    #     MOLECULE_PARENT_DIR = '/'.join(par_dir)
-    #     for line in self.read():
-    #         if 'Optimization completed' in line:
-    #             found_equil = True
-    #         if 'Standard orientation' in line:
-    #             found_some = True
-    #             if len(some_coords) > 0: # from last run, remove those coords
-    #                 some_coords = []
-    #         if found_equil:
-    #             if re.search(regex, line):
-    #                 _, atnum, _, x, y, z = line.split()
-    #                 atnum, x, y, z = map(float, (atnum, x, y, z))
-    #                 sym = PT.get_symbol(atnum)
-    #                 coords.append(Atom(sym, coords = (x, y, z)))
-    #         if found_some:
-    #             if re.search(regex, line):
-    #                 _, atnum, _, x, y, z = line.split()
-    #                 atnum, x, y, z = map(float, (atnum, x, y, z))
-    #                 sym = PT.get_symbol(atnum)
-    #                 some_coords.append(Atom(sym, coords = (x, y, z)))
-    #         if 'Rotational constants' in line:
-    #             found_some = False
-    #         if 'Distance matrix (angstroms)' in line:
-    #             found_equil = False
-    #     if len(coords) > 0:
-    #         print('found!')
-    #         write_xyz(coords, os.path.join(MOLECULE_PARENT_DIR, 'equil.xyz'))
-    #     else:
-    #         if len(some_coords) > 0:
-    #             print(f'not found.\nNeeds resubmitting. Coords stored in {self.path}/rerun.xyz')
-    #             write_xyz(some_coords, os.path.join(MOLECULE_PARENT_DIR, 'rerun.xyz'))
-    #             # rerun_dir = os.path.join(self.path, 'rerun')
-    #             # if not os.path.exists(rerun_dir):
-    #             # # if already exists, then simulation already re-run- skip this log, move to next
-    #             #     os.mkdir(rerun_dir)
-    #             #     write_xyz(some_coords, os.path.join(rerun_dir, 'rerun.xyz'))
-    #         else:
-    #             print('No iterations were cycled through!')
-
-
     def is_optimisation(self):
         return 'opt' in self.get_runtype()
 
