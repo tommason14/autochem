b336b84357459414c9957afe6b50e88a0614ebff
tommason14 <tom.mason14@gmail.com>
2020-02-23 14:34:50 +1100

    f string needed

--- chem_assistant/interfaces/gamess.py
+++ chem_assistant/interfaces/gamess.py
@@ -496,10 +496,9 @@ energy (spec) or hessian matrix calculation for thermochemical data and vibratio
             .replace("mem=96", f"mem={str(self.meta.mem).upper().replace('GB', '')}")
             .replace("ncpus=48", f"ncpus={self.meta.ncpus}")
             .replace("jobfs=100", f"jobfs={str(self.meta.jobfs).upper().replace('GB', '')}")
-            # partition
         )
         if 'partition' in self.meta:
-            job = job.replace('#PBS -l wd', '#PBS -l wd\n#PBS -q {self.meta.partition}')
+            job = job.replace('#PBS -l wd', f'#PBS -l wd\n#PBS -q {self.meta.partition}')
         return job
 
     def create_job(self):
