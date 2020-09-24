4190e4882bd0a60925541fedc904d4b2816aa7b6
Tom Mason <tom.mason14@gmail.com>
2020-09-24 23:01:05 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ command -v gfortran > /dev/null && fort=gfortran || fort=ifort
 [ -f ~/.zshrc ] && rc=~/.zshrc || rc=~/.bashrc
 echo "Compiling autochem/core/thermo.f"
 $fort autochem/core/thermo.f -o autochem/core/thermo.exe
-echo "Setting PYTHONPATH in $rc"
+echo "Adding to PATH and PYTHONPATH in $rc"
+echo "export PATH=\"$(pwd)/bin:\$PATH\"" >> $rc
 echo "export PYTHONPATH=\"$(pwd):\$PYTHONPATH\"" >> $rc
