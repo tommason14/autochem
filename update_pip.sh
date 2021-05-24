43388a85ae855288c2d120f5a4bedaa653a9a88d
Tom Mason <tom.mason14@gmail.com>
2021-05-24 17:59:42 +1000

    Delete update_pip.sh

--- update_pip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-rm -r autochem.egg-info build dist
-printf "Reminder to increment version number in setup.py. Press any key to continue"
-read
-vim setup.py
-python3 setup.py bdist_wheel sdist
-printf "Upload to pip? [y/n] "
-read 
-[ $REPLY = 'y' ] || [ $REPLY = 'Y' ] && twine upload dist/*
